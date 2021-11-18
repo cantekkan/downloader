f3857b6be165be43b54c0862a0aa2ea7f17da98d
cantekkan <cantekkan@yahoo.com.tr>
2021-11-18 13:17:51 +0300

    ilkdownload ornegini yaptim

---
+++ downloader/downloader/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace downloader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //
+
+            var x = new indirici();
+            x.IndirmedeDegisiklikoldugunda += X_IndirmedeDegisiklikoldugunda;
+            x.IndirmeBittiginde += X_IndirmeBittiginde;
+
+            x.indir("http://cdn.smg.com.tr/x00078c09-37a1-4ab1-bd37-c0ce48a0dc76.mp3","deneme.mp3");
+
+            Console.ReadLine();
+        }
+
+        private static void X_IndirmeBittiginde(bool iptalEdildiMi, string durum)
+        {
+            Console.WriteLine($"bitis bilgisi geldi, iptal mi:{iptalEdildiMi}, durum:{durum}");
+        }
+
+        private static void X_IndirmedeDegisiklikoldugunda(int yuzde,long indirilen, long total)
+        {
+            Console.WriteLine($"{yuzde} degisiklik oldu , {indirilen} / {total}");
+        }
+    }
+}
---
+++ downloader/downloader/eventOrnegi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace downloader
+{
+    public class indirici
+    {
+
+        public delegate void IndirmeDurumuEventi(int yuzde, long indirilen, long total);
+        public event IndirmeDurumuEventi IndirmedeDegisiklikoldugunda;
+
+        public delegate void indirmeBittiEventi(bool iptalEdildiMi, string durum);
+        public event indirmeBittiEventi IndirmeBittiginde;
+
+        public void indir(string url, string hedefKonum)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.DownloadProgressChanged += WebClient_DownloadProgressChanged;
+                webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
+
+                webClient.DownloadFileAsync(new Uri(url), hedefKonum);
+
+                 
+            }
+        }
+
+
+
+        public void WebClient_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (e.Error == null && !e.Cancelled)
+            {
+                Console.WriteLine("Download completed!");
+            }
+            IndirmeBittiginde(e.Cancelled, "basari ile bitti");
+        }
+
+        public void WebClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Console.WriteLine("Downloaded:" + e.ProgressPercentage.ToString());
+            IndirmedeDegisiklikoldugunda(e.ProgressPercentage,e.BytesReceived,e.TotalBytesToReceive );
+        }
+
+
+    }
+}
